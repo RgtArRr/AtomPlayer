f52f9dfa5e0f7cb2938ff98f3e416d457ff6b1db
kenneth <koga262@gmail.com>
2016-07-26 18:29:35 -0500

    add Icono

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600, frame: false, minWidth: 400, minHeight: 300});
+  win = new BrowserWindow({icon: __dirname + '/icon.ico', width: 800, height: 600, frame: false, minWidth: 400, minHeight: 300});
 
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`);
