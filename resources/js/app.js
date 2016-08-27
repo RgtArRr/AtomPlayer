8028a3e6ba41c93f61659e3b07a16bb80c09c4f3
Kenneth Junior Obregon Garcia <koga_26@hotmail.com>
2016-08-27 07:22:37 -0500

    load local songs
    Carga canciones del disco en el reproductor

--- resources/js/app.js
+++ resources/js/app.js
@@ -45,23 +45,18 @@ $(document).keydown(function(e) {
 //################
 
 $("#add_Song").click(function(){
-	dialog.showOpenDialog({title: "Escoge una musica",  filters: [{name: 'Canciones', extensions: ['mp3']},]}, function(e){
-		alert("Comming soon");
-		/*
-		var tr = createNode("tr");
-		var td1 = createNode("td");
-		var td2 = createNode("td");
-		var buttonPlay = createNode("button");
-		buttonPlay.html("Play");
-		buttonPlay.click(function(){
-		player.load("file:///"+e[0]);
-	});
-	td1.append(e[0].split("\\")[e[0].split("\\").length-1]);
-	td2.append(buttonPlay);
-	tr.append(td2);
-	tr.append(td1);
-	$("#canciones").append(tr);
-	*/
+	dialog.showOpenDialog({title: "Escoge una musica", properties: ['openFile', 'multiSelections'],  filters: [{name: 'Canciones', extensions: ['mp3']},]}, function(e){
+		if(e){
+			$.each(e, function(j,k){
+				//Check extension file
+				if(k.substr(k.length - 4) == ".mp3"){
+					var name = k.split("\\")[k.split("\\").length - 1];
+					name = name.substr(0, name.length-4);
+					db.addSong(name, k, 1);
+					$("#playlist_default").click();
+				}
+			});
+		}
 });
 });
 
@@ -76,6 +71,11 @@ $("#download").click(function(){
 $("#minimize").click(function(){
 	ipcRenderer.sendSync('toogleMode', {});
 });
+
+$("#settings").click(function(){
+	ipcRenderer.sendSync('openSettings', {});
+});
+
 
 //Temporal Playlist event
 //******
@@ -142,6 +142,8 @@ $("#playlist_default").click(function(){
 			tr.data("id_song", k[0]);
 			$("#canciones").append(tr);
 		});
+		//Change view
+		toogleView("playlist");
 	}
 });
 
@@ -216,150 +218,6 @@ function ContextMenu(){
 	this.destroy = function(){
 		this.menu.remove();
 	}
-}
-
-
-function YTdownloader(){
-	var self = this;
-	//Cambiar variable por URLinput
-	this.input = {div: createNode("div", {class: "form-group"}), label: createNode("label", {html: "Descargar musica en .mp3 de YouTube"}), input : createNode("input", {type: "text", class: "form-control", placeholder: "Copie un link de youtube aqui"})};
-	this.input.div.append(this.input.label);
-	this.input.div.append(this.input.input);
-
-	this.button = createNode("button", {class: "btn btn-form btn-primary", html: "Descargar"});
-
-	this.barProgress = {container: createNode("div", {"class":"progress"}), main: createNode("div", {class: "c100"}), title: createNode("span"),back: createNode("div", {"class": "back"}), comp1: createNode("div", {class: "slice"}), comp2: createNode("div", {class: "bar"}), comp3: createNode("div", {class: "fill"})}
-	this.barProgress.comp1.append(this.barProgress.comp2);
-	this.barProgress.comp1.append(this.barProgress.comp3);
-	this.barProgress.main.append(this.barProgress.title);
-	this.barProgress.main.append(this.barProgress.comp1);
-	this.barProgress.main.append(this.barProgress.back);
-	this.barProgress.container.append(this.barProgress.main);
-
-	this.divContainer = createNode("div", {"class": "ytdownloadercontainer"});
-	this.divContainer.append(this.input.div);
-	this.divContainer.append(this.button);
-	this.divContainer.append(this.barProgress.container);
-
-	this.iscancelled = false;
-
-	this.draw = function(element){
-		this.barProgress.main.hide();
-
-		this.barProgress.back.click(function(){
-			self.iscancelled = true;
-			self.input.input.val("");
-			self.toogleView();
-		});
-
-		this.button.click(function(){
-			var url = urlObject({"url" : self.input.input.val()});
-			var idvideo = null;
-			switch(url.hostname) {
-				case "www.youtube.com":
-				case "youtube.com":
-				idvideo = url.parameters["v"];
-				break;
-				case "www.youtu.be":
-				case "youtu.be":
-				idvideo = url.pathname.split("/")[1];
-				break;
-				default:
-				console.log("No es una url de YouTube");
-			}
-			if(idvideo){
-				self.toogleView();
-				self.iscancelled = false;
-				//Reset barprogress
-				self.barProgress.main.removeClass();
-				self.barProgress.main.addClass("c100");
-				self.barProgress.title.html("0%");
-				//----------------
-				convert2mp3(idvideo, function(result){
-					self.barProgress.back.html("Cancelar descarga");
-					//Quitamos el # cuando hay caracteres especiales
-					result[3] = result[3].replace("#", "");
-					//Sacamos todos los caracteres que Windows no permite
-					result[3] = result[3].replace("/","").replace("\\","").replace(":","").replace("*","").replace("?","").replace("\"","").replace("|","").replace("<","").replace(">","").replace("?","");
-					//----------------------------------------------
-
-					//Obtener nombre del archivo, si existen duplicados
-					var b = true;
-					var name = app.getPath("music")+"/"+result[3];
-					var path = name;
-					var cont = 1;
-					while(b){
-						if(!fs.existsSync(path +".mp3")){
-							b= false;
-						}else{
-							path = name + " [" + cont + "]";
-							cont++;
-						}
-					}
-					//-------------------------------------------------
-					var file = fs.createWriteStream(path +".mp3");
-					var request = http.get("http://dl"+result[1]+".downloader.space/dl.php?id="+result[2], function(response) {
-						var len = parseInt(response.headers['content-length'], 10);
-						var cur = 0;
-						var total = len / 1048576; //1048576 - bytes in  1Megabyte
-						response.on("data", function(chunk) {
-							file.write(chunk);
-							cur += chunk.length;
-							self.updateProgress((100.0 * cur / len).toFixed());
-							if(self.iscancelled){
-								response.destroy();
-							}
-							//console.log("Downloading " + (100.0 * cur / len).toFixed(2) + "% " + (cur / 1048576).toFixed(2) + " mb" + ".Total: " + total.toFixed(2) + " mb");
-						});
-
-						response.on("end", function() {
-							if(!self.iscancelled){
-								file.end();
-								db.addSong(result[3], path+".mp3", 1);
-								self.barProgress.back.html("Descargar otro");
-								console.log("finish");
-							}else{
-								file.emit('close');
-								self.iscancelled = false;
-								console.log("Ha sido cancelado");
-							}
-						});
-
-						request.on("error", function(e){
-							self.failedDownload();
-							console.log("Download error: " + e.message);
-						});
-					});
-				});
-			}else{
-				self.failedDownload();
-			}
-		});
-		element.append(this.divContainer);
-	};
-
-	this.toogleView = function(){
-		if(this.barProgress.main.is(":visible")){
-			this.barProgress.main.hide();
-			this.input.div.show();
-			this.button.show();
-		}else{
-			this.barProgress.main.show();
-			this.input.div.hide();
-			this.button.hide();
-		}
-	};
-
-	this.failedDownload = function(){
-		self.barProgress.back.html("Intentar nuevamente");
-	};
-
-	this.updateProgress = function(percent){
-		self.barProgress.main.removeClass();
-		self.barProgress.main.addClass("c100");
-		self.barProgress.main.addClass("p"+percent);
-		self.barProgress.title.html(percent+"%");
-	};
 }
 
 function TableList(){
